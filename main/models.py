5ec19faf7719cf1aac04e65f3a8f1330e81f1d51
ArturKryazhev <gominers7@gmail.com>
2023-11-21 23:12:30 +0300

    final

--- main/models.py
+++ main/models.py
@@ -9,4 +9,17 @@ class Contact_us(models.Model):
 
 class Notifications(models.Model):
     e_mail = models.EmailField(verbose_name='E-mail')
+
+class Employee(models.Model):
+    second_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    name = models.CharField(max_length=255, verbose_name='Имя')
+    father_name = models.CharField(max_length=255, verbose_name='Отчество', blank= True, null= True)
+    photo = models.ImageField()
+    description = models.TextField(verbose_name='Описание работы')
+    photo = models.ImageField(verbose_name='Фотография')
+    education = models.CharField(max_length=255, verbose_name='Образование')
+    date_education = models.CharField(max_length=4, verbose_name='Дата окончания образования')
+    experience = models.CharField(max_length=50, verbose_name='Опыт работы')
+    place_work = models.CharField(max_length=255, verbose_name='Место работы')
+    work_address = models.CharField(max_length=255, verbose_name='Адрес работы')
 # Create your models here.
